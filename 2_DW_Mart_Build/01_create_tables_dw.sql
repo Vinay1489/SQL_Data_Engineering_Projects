40335d2c0ffb84befe1ad7669d6a99484689ded3
Sai Vinay <vinayvennampally@gmail.com>
2026-06-12 08:35:27 +0530

    Create Star schema tables for data warehouse

--- 2_DW_Mart_Build/01_create_tables_dw.sql
+++ 2_DW_Mart_Build/01_create_tables_dw.sql
@@ -1,2 +1,52 @@
 -- Step 1: 🏗️ DW - Create star schema tables
 
+drop table if exists skills_job_dim;
+drop table if exists job_postings_fact;
+drop table if exists company_dim;
+drop table if exists skills_dim;
+
+Create table company_dim(
+    company_id integer primary key,
+    name varchar,
+);
+
+Create table skills_dim(
+    skill_id integer primary key,
+    skill varchar,
+    type varchar
+);
+
+Create table job_postings_fact(
+    job_id integer primary key,
+    company_id integer,
+    job_title_short varchar,
+    job_title varchar,
+    job_location varchar,
+    job_via varchar,
+    job_schedule_type varchar,
+    job_work_from_home varchar,
+    search_location varchar,
+    job_posted_date timestamp,
+    job_no_degree_mention boolean,
+    job_health_insurance boolean,
+    job_country varchar,
+    salary_rate varchar,
+    salary_year_avg double,
+    salary_hour_avg double,
+    foreign key (company_id) references company_dim(company_id)
+);
+
+
+Create table skills_job_dim(
+    skill_id integer,
+    job_id integer,
+    primary key(skill_id,job_id),
+    foreign key(skill_id) references skills_dim(skill_id),
+    foreign key(job_id) references job_postings_fact(job_id)
+);
+
+
+select 'Tables created Successfully!';
+
+select table_name from information_schema.tables
+where table_schema='main';
